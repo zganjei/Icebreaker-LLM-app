d0a69430b001e82a867509e34911aff35b8bd1b6
Zeinab Ganjei <zzganjei@gmail.com>
2025-04-30 15:05:22 +0200

    output parser added

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 
 def ice_break_with(name:str) -> str:
     linkedin_url = linkedin_lookup_agent(name=name)
@@ -12,16 +13,19 @@ def ice_break_with(name:str) -> str:
     given the LinkedIn information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
+
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], 
+        template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()}
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")    
-    
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")        
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedIn_data})
 
     print(res)
@@ -30,5 +34,5 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Zeinab Ganjei sweden")
+    ice_break_with(name="Börje Ekholm sweden")
 
